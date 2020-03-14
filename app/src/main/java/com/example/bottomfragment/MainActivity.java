2bbd29c4f889422452019445d55d3446ec80b918
Iron <perts.pasha.pro@gmail.com>
2020-03-14 04:41:17 +0200

    Some edits

--- app/src/main/java/com/example/bottomfragment/MainActivity.java
+++ app/src/main/java/com/example/bottomfragment/MainActivity.java
@@ -21,7 +21,6 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
 
-        //I added this if statement to keep the selected fragment when rotating the device
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                     new HomeFragment()).commit();
