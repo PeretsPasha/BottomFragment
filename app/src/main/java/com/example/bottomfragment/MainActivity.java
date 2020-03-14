f0112520b30d79d42e170ce868ac10fe7d205478
Iron <perts.pasha.pro@gmail.com>
2020-03-14 04:36:24 +0200

    Some edits

--- app/src/main/java/com/example/bottomfragment/MainActivity.java
+++ app/src/main/java/com/example/bottomfragment/MainActivity.java
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.os.Bundle;
 import android.view.MenuItem;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
 
     //comment
